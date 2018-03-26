3af72ef27bd8c90df8f92a7023596df5d5b741bd
Hong Zhou <zhsnnu@gmail.com>
2018-03-25 22:00:49 -0500

    add Spirng-data-rest dependency

--- src/main/java/com/hongzhou/university/repo/ReadOnlyRepository.java
+++ src/main/java/com/hongzhou/university/repo/ReadOnlyRepository.java
@@ -1,5 +1,21 @@
 package com.hongzhou.university.repo;
 
-public interface ReadOnlyRepository {
+import java.io.Serializable;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.repository.Repository;
+
+@NoRepositoryBean
+public interface ReadOnlyRepository<T, ID extends Serializable> extends Repository<T, ID> {
+	
+	T findOne(ID id);
+	Iterable<T> findAll();
+	Iterable<T> findAll(Iterable<ID> ids);
+	Page<T> findAll(Pageable pageable);
+	Iterable<T> findAll(Sort sort);
+	boolean exists(ID id);
+	long count();
 }
